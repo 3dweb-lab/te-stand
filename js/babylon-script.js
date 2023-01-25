188c136b954c34d86a53bee79dfacad7d156eaef
Vadim S <115616942+3dweb-lab@users.noreply.github.com>
2023-01-25 17:38:36 +0100

    Add files via upload

--- js/babylon-script.js
+++ js/babylon-script.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("renderCanvas"); // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
-renderCanvas.addEventListener("wheel", evt => evt.preventDefault());
+canvas.addEventListener("wheel", evt => evt.preventDefault());
 
 const rootUrl = "assets/models/";
 const fileName = "TE_for_glb_05.glb";
@@ -89,6 +89,7 @@ const createScene = function () {
 
         container.addAllToScene();
 
+        // Focus
         const boundingbox = scene.getMeshByName("_BoundingBox");
         if (boundingbox)
         {
@@ -100,24 +101,173 @@ const createScene = function () {
             camera.setTarget(new BABYLON.Vector3.Zero());
         }
 
-        console.log(cameras);
+        // GUI
+        // Create advance texture
+        var advancedTexture = new BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI", scene);
+        advancedTexture.renderScale = 1;
 
+        //advancedTexture.BILINEAR_SAMPLINGMODE = 32;
+        advancedTexture.LINEAR_NEAREST = 12;
+
+        // advancedTexture.idealWidth = 1600;
+        // advancedTexture.renderAtIdealSize = true;
+        // if( window.innerWidth < 1000 ) {
+        //     hotspot.fontSize = 0;
+        //     return scene;
+        // }
+
+        let images = [];
+        var currentIndex = 0;
+        // Create Hotspots in 3d scene
+        var hs = scene.getTransformNodeByName("_Hotspots").getChildTransformNodes();
+        hs.forEach(spot => {
+            var hotspot = BABYLON.GUI.Button.CreateSimpleButton("button", spot.name);
+            spot.setEnabled(false);
+            hotspot.width = "24px";
+            hotspot.height = "24px";
+            hotspot.cornerRadius = 40;
+            hotspot.background = "white";
+            hotspot.color = "#888";
+            hotspot.thickness = 0;
+            hotspot.alpha= 0.75;
+            hotspot.fontSize = 20;
+            hotspot.fontFamily = "Roboto";
+            
+            // Make Hotspot buttons show image
+            images.push("assets/images/step1/"+spot.name+".jpg");
+            hotspot.onPointerUpObservable.add((evt) => {
+                showImage(spot.name-1);
+            });
+            advancedTexture.addControl(hotspot);
+            hotspot.linkWithMesh(spot);
+        });
+
+        
+        var image = new BABYLON.GUI.Image("image", "");
+        image.stretch = BABYLON.GUI.Image.STRETCH_UNIFORM;
+        image.height = 0.8;
+        advancedTexture.addControl(image);
+        image.isVisible = false;
+
+        image.onPointerClickObservable.add((evt) => {
+            closeImage();
+        });
+
+        function showImage(index) {
+            image.source = images[index];
+            image.shadowBlur = canvas.height/4;
+            image.isVisible = true;
+            image.scaleX = 1;
+            image.scaleY = 1;
+            image.zIndex = 5;
+            image.leftInPixels = 0;
+            image.topInPixels = 0;
+        };
+
+        function closeImage() {     
+            image.isVisible = false;
+            image.shadowBlur = 0;
+        }
+
+        // SCREENSHOT
+        var screenshot = document.getElementById('screenShot');
+        screenshot.addEventListener('click',function(){
+            BABYLON.Tools.CreateScreenshot(engine, camera, {width: canvas.width, height: canvas.height});
+        });
+        console.log(cameras);
+        // CAMERA SELECTOR
+        var select = document.getElementById("camerasList");
         for (var i=0; i < cameras.length; i++) {
-            var select = document.getElementById("camerasList");
             var newOption = document.createElement("option");
-            newOption.value = cameras[i].id;
+            //newOption.value = cameras[i].id;
+            newOption.id = cameras[i].id;
             newOption.text = cameras[i].id;
             select.appendChild(newOption);
         }
         
         // select.addEventListener('change', () => {
-        //     scene.activeCamera = select.option;
+        //     scene.activeCamera = cameras[select.options.selected];
         // });
 
         
         //scene.activeCamera = select.value;
-    });
 
+        // POST PROCESSING
+        // Motion blur
+        var motionblur = new BABYLON.MotionBlurPostProcess(
+            'motionblur', // The name of the effect.
+            scene, // The scene containing the objects to blur according to their velocity.
+            1.0, // The required width/height ratio to downsize to before computing the render pass.
+            scene.activeCamera // The camera to apply the render pass to.
+        );
+        motionblur.motionStrength = 0.5; // amout of the blur effect
+        motionblur.motionBlurSamples = 16;
+        motionblur.isObjectBased = false; // Disable object-based mode in order to enable screen-based mode.
+        scene.activeCamera.detachPostProcess(motionblur);
+
+        var inputMB = document.getElementById('toggleMB');
+        inputMB.addEventListener('change',function(){
+            if(this.checked) {
+                scene.activeCamera.attachPostProcess(motionblur)
+            } else {
+                scene.activeCamera.detachPostProcess(motionblur)
+            }
+        });
+
+        // Create Default RenderPipeline
+        var defaultPipeline = new BABYLON.DefaultRenderingPipeline("DefaultRenderingPipeline", false, scene, scene.cameras);
+        if (defaultPipeline.isSupported) {
+            defaultPipeline.bloomEnabled = false;
+            defaultPipeline.fxaaEnabled = false;
+            defaultPipeline.samples = 1;
+            
+            // MSAA
+            // var inputMSAA = document.getElementById('toggleMsaa');
+            // inputMSAA.addEventListener('change',function(){
+            //     if(this.checked) {
+            //         defaultPipeline.samples = 4;
+            //     } else {
+            //         defaultPipeline.samples = 1;
+            //     }
+            // });
+            // FXAA
+            var inputFXAA = document.getElementById('toggleFxaa');
+            inputFXAA.addEventListener('change',function(){
+                if(this.checked) {
+                    defaultPipeline.fxaaEnabled = true;
+                    defaultPipeline.fxaa.samples = 8;
+                    defaultPipeline.fxaa.adaptScaleToCurrentViewport = true;
+                } else {
+                    defaultPipeline.fxaaEnabled = false;
+                }
+            });
+            // Sharpening
+            var inputSharp = document.getElementById('toggleSharpening');
+            inputSharp.addEventListener('change',function(){
+                if(this.checked) {
+                    defaultPipeline.sharpenEnabled = true;
+                } else {
+                    defaultPipeline.sharpenEnabled = false;
+                }
+            });
+            defaultPipeline.sharpen.edgeAmount = 0.2;
+            defaultPipeline.sharpen.colorAmount = 1.0;
+            // Bloom
+            var inputBloom = document.getElementById('toggleBloom');
+            inputBloom.addEventListener('change',function(){
+                if(this.checked) {
+                    defaultPipeline.bloomEnabled = true;
+                } else {
+                    defaultPipeline.bloomEnabled = false;
+                }
+            });
+            defaultPipeline.bloomThreshold = 0.9;
+            defaultPipeline.bloomWeight = 0.15;
+            defaultPipeline.bloomKernel = 64;
+            defaultPipeline.bloomScale = 0.5;
+        }
+    });
+    //engine.setHardwareScalingLevel(.5);
     engine.hideLoadingUI();
     return scene;
 };
