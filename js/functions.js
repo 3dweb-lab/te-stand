c8bae87e01de1eeff52538a62d3cc2598c31db8a
Vadim S <115616942+3dweb-lab@users.noreply.github.com>
2023-01-27 17:32:51 +0100

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,5 @@
+const password = "vrpe";
+
 document.addEventListener('DOMContentLoaded', function() {
     const panel = document.getElementById("controlPanel");
     const toggleButton = document.getElementById("cameraClickableTag");
@@ -6,4 +8,38 @@ document.addEventListener('DOMContentLoaded', function() {
         toggleButton.classList.toggle("open");
         panel.classList.toggle("open");
     });
+
+    const loginPage = document.getElementById("loginPage");
+    const inputPsw = document.getElementById("psw");
+    const enterButton = document.getElementById("loginBtn");
+    const body = document.getElementById("body");
+
+    enterButton.addEventListener("click", function() {
+        validateLogin();
+    });
+
+    inputPsw.addEventListener("keypress", function(event) {
+        // If the user presses the "Enter" key on the keyboard
+        if (event.key === "Enter") {
+          // Cancel the default action, if needed
+          event.preventDefault();
+          // Trigger the button element with a click
+          validateLogin();
+        }
+      });
+
+    function validateLogin() {
+        if (inputPsw.value === password) {
+            //alert("Login successful!");
+            body.style.overflowY = "scroll";
+            body.style.overflowX = "hidden";
+            loginPage.style.display = "none";
+        }
+        else if (inputPsw.value == "") {
+            //alert("Enter password!");
+        } 
+        else {
+            alert("Login failed!");
+        }
+    }
 });
